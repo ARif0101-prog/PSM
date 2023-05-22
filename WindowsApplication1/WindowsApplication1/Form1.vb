8fdb19226f553d4dcda88626a11b44f8281c80b0
ARif0101-prog <57076754+ARif0101-prog@users.noreply.github.com>
2023-05-22 11:51:58 +0700

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            SerialPort1.Open()
+            Label1.Text = "Connected"
+        Catch ex As Exception
+            Label1.Text = "Not connected"
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If SerialPort1.IsOpen Then
+            SerialPort1.WriteLine("ON")
+        Else
+            MsgBox("Serial port not connected")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If SerialPort1.IsOpen Then
+            SerialPort1.WriteLine("OFF")
+        Else
+            MsgBox("Serial port not connected")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            SerialPort1.Close()
+            Label1.Text = "Not connected"
+        Catch ex As Exception
+            Label1.Text = "connected"
+        End Try
+    End Sub
+End Class

b4735529c5b52f28221e8fcbf75b813de716a2f1
ARif0101-prog <57076754+ARif0101-prog@users.noreply.github.com>
2023-06-21 00:33:14 +0700

    Update pub_esp8266.ino

--- pub_esp8266.ino
+++ pub_esp8266.ino
@@ -1,104 +1,71 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <Ticker.h>
-#include <AsyncMqttClient.h>
-
-//replace with your network credentials
-#define WIFI_SSID "testtt"
-#define WIFI_PASSWORD "testtest"
-
-// Raspberry Pi Mosquitto MQTT Broker
-#define MQTT_HOST "broker.emqx.io" //IPAddress(192, 168, 1, XYZ)
-#define MQTT_PORT 1883
-
-//MQTT Topic
-#define MQTT_PUB_Output "esp8266/OutputControl"
-
-const int PushButton = 0;
+#include <PubSubClient.h>
 int Push_button_state;
 bool Push_button_Prv_state = false;
+const int PushButton = 0;
+const char *WIFI_SSID = "testtt";
+const char *WIFI_PASSWORD = "testtest";
 
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
+const char *MQTT_HOST = "broker.emqx.io";
+const int MQTT_PORT = 1883;
+const char *MQTT_CLIENT_ID = "ESP8266 NodeMCU";
+const char *MQTT_USER = "";
+const char *MQTT_PASSWORD = "";
+const char *TOPIC = "nodemcu/test";
 
-WiFiEventHandler wifiConnectHandler;
-WiFiEventHandler wifiDisconnectHandler;
-Ticker wifiReconnectTimer;
+WiFiClient client;
+PubSubClient mqttClient(client);
 
+void setup()
+{
+    Serial.begin(9600);
 
-void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("Connected to Wi-Fi");
+
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
+    while (!client.connected()) {
+        if (mqttClient.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD )) {
+            Serial.println("Connected to MQTT broker");
+        } else {
+            delay(500);
+            Serial.print(".");
+        }
+    }
 }
 
-void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  Serial.println("Connected to Wi-Fi.");
-  connectToMqtt();
-}
-
-void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  Serial.println("Disconnected from Wi-Fi.");
-  mqttReconnectTimer.detach();
-  wifiReconnectTimer.once(2, connectToWifi);
-}
-
-void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
-}
-
-void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
-  }
-}
-
-void onMqttPublish(uint16_t packetId) {
-  Serial.print("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  pinMode(PushButton, INPUT);
-
-  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
-  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
-
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onPublish(onMqttPublish);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-
-  connectToWifi();
-}
-
-void loop() {
+void loop()
+{
   Push_button_state = digitalRead(PushButton);
-  if (Push_button_state == HIGH && Push_button_Prv_state == false)
+ if (Push_button_state == HIGH && Push_button_Prv_state == false)
   {
-    // Publish an MQTT message on topic esp8266/OutputControl
-    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_Output, 1, true, "true");
-    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_Output, packetIdPub1);
-    Serial.println(" Message: ON");
+    mqttClient.publish(TOPIC,String("true").c_str());
+    // // Publish an MQTT message on topic esp8266/OutputControl
+    //  uint16_t packetIdPub1 = mqttClient.publish(TOPIC,("true").c_str());//uint16_t packetIdPub1 = mqttClient.publish(TOPIC, 1,"true");
+    //  Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", TOPIC;, packetIdPub1);
+    //  Serial.println(" Message: ON");
     Push_button_Prv_state = true;
   }
   else if (Push_button_state == LOW && Push_button_Prv_state == true)
   {
+    mqttClient.publish(TOPIC,String("false").c_str());
+    //mqttClient.publish(TOPIC,"false");
     // Publish an MQTT message on topic esp8266/OutputControl
-    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_Output, 1, true, "false");
-    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_Output, packetIdPub2);
-    Serial.println(" Message: OFF");
+    // uint16_t packetIdPub2 = mqttClient.publish(TOPIC,("false").c_str());// uint16_t packetIdPub2 = mqttClient.publish(TOPIC, 1,"false");
+    //  Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", TOPIC, packetIdPub2);
+    //  Serial.println(" Message: OFF");
     Push_button_Prv_state = false;
   }
-  delay(1000);
+
+    // int value = random(1, 101);
+    // mqttClient.publish(TOPIC, String(value).c_str());
+
+    // delay(1000);
 }
